3ebb50474ef493b31d04aaf53afe0826b18d1a2f
marcus <97414481+kiryano@users.noreply.github.com>
2023-07-13 00:28:46 -0700

    Redid a lot/the duplicate detection & more.
    Redid a lot/the duplicate detection & more.

--- avatar_organizer.py
+++ avatar_organizer.py
@@ -2,8 +2,8 @@ import os
 import shutil
 import random
 import string
-from PIL import Image
-from colorama import Fore, Style
+import cv2
+from colorama import Fore
 import logging
 import pyfiglet
 import argparse
@@ -27,7 +27,6 @@ parser.add_argument('-t', '--target', default='avatars', help='Specify the targe
 parser.add_argument('-c', '--confirm', action='store_true', help='Prompt for confirmation before moving files')
 args = parser.parse_args()
 
-# Check if the target folder already exists
 target_folder = args.target
 if not os.path.exists(target_folder):
     try:
@@ -45,7 +44,6 @@ if args.confirm:
         print(Fore.RED + 'Operation cancelled.')
         exit()
 
-# Create the backup directory if it doesn't exist
 backup_dir = 'backup'
 if not os.path.exists(backup_dir):
     try:
@@ -54,7 +52,7 @@ if not os.path.exists(backup_dir):
         logging.error(f'Error creating the backup directory: {str(e)}')
         print(Fore.RED + f'Error creating the backup directory: {str(e)}')
 
-photo_extensions = ['jpg', 'jpeg', 'png', 'gif']
+file_extensions = ['jpg', 'jpeg', 'png', 'gif']
 
 
 # Recursive function to search for avatars in subdirectories
@@ -62,11 +60,11 @@ def search_avatars(directory):
     avatars = []
     for root, dirs, files in os.walk(directory):
         for file in files:
-            if file.lower().endswith(tuple(photo_extensions)):
+            if file.lower().endswith(tuple(file_extensions)):
                 avatars.append(os.path.join(root, file))
     return avatars
 
-# Calculate file hash
+
 def calculate_file_hash(file_path):
     hasher = hashlib.md5()
     with open(file_path, 'rb') as file:
@@ -75,43 +73,59 @@ def calculate_file_hash(file_path):
     return hasher.hexdigest()
 
 
+def verify_image(file_path):
+    try:
+        image = cv2.imread(file_path)
+        if image is None:
+            return False
+        return True
+    except cv2.error as error:
+        logging.warning(f'Error reading image file: {file_path} ({str(error)})')
+        print(Fore.RED + f'Warning: Error reading image file: {file_path} ({str(error)})')
+        return False
+    except Exception as error:
+        logging.warning(f'Error processing image file: {file_path} ({str(error)})')
+        print(Fore.RED + f'Warning: Error processing image file: {file_path} ({str(error)})')
+        return False
+
+
 # Find all avatars in the current directory and subdirectories
 avatars = search_avatars('.')
 
-# Check if any avatars were found
 if len(avatars) == 0:
-    logging.error('No avatars were found with the specified extensions.')
-    print(Fore.RED + 'Error: No avatars found with the specified extensions.')
+    logging.error('No files found with the specified extensions.')
+    print(Fore.RED + 'Error: No files found with the specified extensions.')
     exit()
 
-# Sort the avatars based on modification date in ascending order
-sorted_files = sorted(avatars, key=os.path.getmtime)
-
-# Move each valid photo file to the target folder with a random filename
-encountered_hashes = set()
-for file in sorted_files:
+encountered_images = []
+for file in avatars:
     try:
-        # This will validate the image
-        with Image.open(file) as img:
-            img.verify()
-
-        # Check for duplicate file
-        file_hash = calculate_file_hash(file)
-        if file_hash in encountered_hashes:
-            logging.warning(f'Duplicate file found: {file}')
-            print(f'Warning: Duplicate file found: {file}')
+        if not verify_image(file):
+            logging.warning(f'Invalid file: {file}')
+            print(Fore.RED + f'Warning: Invalid file: {file}')
             continue
-        encountered_hashes.add(file_hash)
 
-        # Create a backup of the file in the backup directory
+        # Calculate file hash for fast preliminary duplicate detection
+        file_hash = calculate_file_hash(file)
+
+        # Check for potential duplicates based on file hash
+        if file_hash in encountered_images:
+            logging.warning(f'Duplicate image found: {file}')
+            print(Fore.RED + f'Warning: Duplicate image found: {file}')
+            os.remove(file)  # Delete the duplicate file
+            continue
+
+        encountered_images.append(file_hash)
+
+        # Create a backup of the files in the backup directory
         try:
-            shutil.copy(file, os.path.join(backup_dir, os.path.basename(file)))
+            shutil.copy2(file, os.path.join(backup_dir, os.path.basename(file)))
             logging.info(f'Created backup: {file} -> {backup_dir}/{os.path.basename(file)}')
         except OSError as e:
             logging.error(f'Error creating the backup file: {file} ({str(e)})')
             print(Fore.RED + f'Error creating the backup file: {file} ({str(e)})')
 
-        # Move the file to the target folder with a random filename
+        # Move the files to the target folder with a random filename
         random_name = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
         new_filename = f'{random_name}.{file.rsplit(".", 1)[1]}'
         try:
@@ -120,8 +134,10 @@ for file in sorted_files:
         except OSError as e:
             logging.error(f'Error moving the file: {file} ({str(e)})')
             print(Fore.RED + f'Error moving the file: {file} ({str(e)})')
-    except (IOError, SyntaxError) as e:
-        logging.warning(f'Invalid photo file: {file} ({str(e)})')
-        print(Fore.YELLOW + f'Warning: Invalid photo file: {file} ({str(e)})')
+
+    except (IOError, SyntaxError, TypeError) as e:
+        logging.warning(f'Invalid file: {file} ({str(e)})')
+        print(Fore.RED + f'Warning: Invalid file: {file} ({str(e)})')
+
 
 print(Fore.BLUE + 'Your avatars have been organized and moved to the target folder with random filenames.')
